f2b7850f8503e68c2d4f43a1bab9a44879833ec4
Johannes Arnstad <johannes@solvr.no>
2024-01-22 10:37:20 +0100

    add service methods to handle videos from url

--- src/services/VideoToolkit.php
+++ src/services/VideoToolkit.php
@@ -10,7 +10,7 @@ use yii\base\Component;
  */
 class VideoToolkit extends Component
 {
-    // get id from youtube video
+    // get id from YouTube video
     public function getYoutubeId($url): string
     {
         $parsedUrl = parse_url($url);
@@ -95,4 +95,72 @@ class VideoToolkit extends Component
         }
         return '';
     }
+
+    // check if youtube or vimeo video, return kind
+    public function getVideoKind($url): string
+    {
+        if (strpos($url, 'youtube') !== false) {
+            return 'youtube';
+        } elseif (strpos($url, 'vimeo') !== false) {
+            return 'vimeo';
+        }
+        return '';
+    }
+
+    // get video embed code
+    public function getVideoEmbedCode($url): string
+    {
+        $kind = $this->getVideoKind($url);
+        if ($kind == 'youtube') {
+            return $this->getYoutubeEmbedCode($url);
+        } elseif ($kind == 'vimeo') {
+            return $this->getVimeoEmbedCode($url);
+        }
+        return '';
+    }
+
+    // get video embed code with responsive wrapper with correct css styles
+    public function getVideoEmbedCodeResponsive($url): string
+    {
+        $kind = $this->getVideoKind($url);
+        if ($kind == 'youtube') {
+            return '<div class="video-wrapper"><div class="video-wrapper-inner">' . $this->getYoutubeEmbedCode($url) . '</div></div>';
+        } elseif ($kind == 'vimeo') {
+            return '<div class="video-wrapper"><div class="video-wrapper-inner">' . $this->getVimeoEmbedCode($url) . '</div></div>';
+        }
+        return '';
+    }
+
+    // get video wrapper css code for inline styling
+    public function getVideoWrapperCss(): string
+    {
+        return '.video-wrapper {
+            position: relative;
+            padding-bottom: 56.25%;
+            height: 0;
+            overflow: hidden;
+        }
+        .video-wrapper-inner {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+        }';
+    }
+
+    // get video wrapper styling as tailwind classes
+    public function getVideoWrapperTailwind(): string
+    {
+        return 'relative pb-[56.25%] h-0 overflow-hidden';
+    }
+
+    // get video wrapper inner styling as tailwind classes
+    public function getVideoWrapperInnerTailwind(): string
+    {
+        return 'absolute top-0 left-0 w-full h-full';
+    }
+
+
+
 }
